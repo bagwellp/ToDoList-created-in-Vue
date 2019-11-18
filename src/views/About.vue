dec765f1e9fc0633728b57f48981a4101622fd22
bagwellp <Stayhome1!>
2019-11-17 21:34:28 -0600

    ToDoList App Created in Vue

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0.0.  I created this
+      using the tutorial Vue crash course on Youtube</p>
+  </div>
+</template>
